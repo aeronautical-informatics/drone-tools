c5323bf30ad8c71730259b922c7037ed9a6b8e0f
Wanja Zaeske <wanja.zaeske@dlr.de>
2021-06-12 10:52:34 +0200

    add desktop items and icons

---
+++ build/blheli_m-configurator/dist/js/blheli_versions.js
@@ -0,0 +1,3 @@
+var BLHELI_VERSIONS_REMOTE = 'https://raw.githubusercontent.com/asizon/blheli-configurator/master/js/blheli_versions.json';
+var BLHELI_VERSIONS_LOCAL = './js/blheli_versions.json';
+var BLHELI_VERSIONS_KEY = 'versions';
--- flake.nix
+++ flake.nix
@@ -3,29 +3,83 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
-    lib = nixpkgs.lib;
-    tools = builtins.map (tool: tool.name) (lib.importTOML ./tools.toml).tools;
-  in rec {
-    packages = lib.genAttrs tools (name:
-    pkgs.stdenv.mkDerivation { 
-      inherit name;
-      src = ./build + "/${name}";
-  
-      nativeBuildInputs = with pkgs; [ makeWrapper ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+        tools = (lib.importTOML ./tools.toml).tools;
+      in rec {
+        packages = builtins.listToAttrs (builtins.map (tool:
+          let
+            name = tool.name;
+            desktopItem = pkgs.makeDesktopItem {
+              inherit name;
+              exec = name;
+              icon = name;
+              comment = tool.comment;
+              desktopName = tool.desktop_name;
+            };
+            pkg = pkgs.stdenv.mkDerivation {
+              inherit name;
+              src = ./build + "/${name}";
 
-      buildPhase = ":";
-      installPhase = ''
-        mkdir -p $out/opt/${name}
-        mv * $out/opt/${name}
-        makeWrapper ${pkgs.nwjs}/bin/nw $out/bin/${name} --add-flags $out/opt/${name}
-      '';
-    });
-     
-    apps = lib.genAttrs tools (name: flake-utils.lib.mkApp { drv = packages."${name}"; });
-    devShell = pkgs.mkShell { buildInputs = with pkgs; [nodejs yarn nodePackages.gulp]; };
-  }
-  );
+              nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+              buildPhase = ":";
+              installPhase = ''
+                install -m 444 -D images/${tool.icon} $out/share/icons/hicolor/128x128/apps/${name}.png
+                mkdir -p $out/opt/${name}
+                mv * $out/opt/${name}
+                makeWrapper ${pkgs.nwjs}/bin/nw $out/bin/${name} --add-flags $out/opt/${name}
+                cp -r ${desktopItem}/share/applications $out/share/
+              '';
+            };
+          in lib.nameValuePair name pkg) tools);
+
+        apps = lib.genAttrs (builtins.map (t: t.name) tools)
+          (name: flake-utils.lib.mkApp { drv = packages."${name}"; });
+
+        checks = packages;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            yq
+            nodejs-10_x
+            (yarn.override { nodejs = nodejs-10_x; })
+            nodePackages.gulp
+          ];
+          shellHook = ''
+            SCRIPT_DIR="$(pwd)"
+
+            get_property(){
+              tomlq -r ".tools[] | select(.name==\"$1\").$2" "$SCRIPT_DIR/tools.toml"
+            }
+
+            mkdir -p repos
+
+            while IFS= read -r name; do
+              if [ -d "$SCRIPT_DIR/build/$name" ] 
+              then
+                echo "skipping $name"
+                continue
+              fi
+              echo "building $name"
+              url=$(get_property $name url)
+              tag=$(get_property $name tag)
+              srcdir="$SCRIPT_DIR/repos/$name"
+              git clone $url $srcdir
+              cd $srcdir
+              git fetch --tags --force --all
+              git checkout $tag
+              yarn install 
+              yarn gulp dist 
+              mkdir -p "$SCRIPT_DIR/build"
+              mv dist "$SCRIPT_DIR/build/$name"
+            done< <(tomlq -r '.tools[].name' tools.toml)
+            exit
+          '';
+        };
+      });
 }
